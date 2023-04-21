9f034ed657d34851502cc395fa1951ecbc82f886
Severo Iglesias <siglesias@ucol.mx>
2023-04-21 14:34:17 -0600

    modify bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let botonSiete = document.querySelector("#elboton");
 let audioIntroCuatro = document.querySelector(".intro-cuatro")		
 botonSiete.addEventListener("click", () => {
+	botonSiete.setAttribute("disabled","disabled")
   let etiquetaAudioSiete = document.createElement("audio");
   etiquetaAudioSiete.setAttribute("src", "./src/recording/0.1introduccionmariayelarbol.m4a");
   etiquetaAudioSiete.play();
